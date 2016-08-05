9405c01c1d8398a99919586edb73fb2fc6f95450
RafaelPaulo <rafael-sq@hotmail.com>
2016-08-05 00:26:49 -0300

    Another test

--- db_config.js
+++ db_config.js
@@ -1,10 +1,11 @@
 'use strict'
 
-const db_string = 'mongodb://localhost/firstApp'
+// const db_string = require('package.json').conf.mogoUrl
 const mongoose  = require('mongoose').connect(db_string)
 const db        = mongoose.connection
 
 db.on('error', console.error.bind(console, 'Erro ao conectar no banco'))
+
 db.once('open', function() {
     var userSchema = mongoose.Schema({
         fullname: String,
